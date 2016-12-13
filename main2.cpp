0534544387637697bb57f75b7bfc03fae69ce69b
karstenbw <karstenb@umich.edu>
2016-12-13 11:57:32 -0500

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,174 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <cctype>
+#include "Thesaurus.h"
+#include "MD2.h"
+#include "MD4.h"
+#include "MD5.h"
+//#include "SHA1.h"
+
+using namespace std;
+
+typedef unordered_map<string, string> stringmap;
+
+/* 	Requires: vector<vector> of strings holding all possible synonyms, 0, empty stringmap,
+empty string, and the hash function
+	Outputs the pairs of Messages that hash to the same value. */
+void makeMap(vector<vector<string> > syns, int num, stringmap& targetHash, string newMsg, MD2 md){
+	if(syns.size() == num){
+		string newHash = md.hash(newMsg).substr(0,9);			// The substring here should be increased as you feel more confident/patient
+		targetHash.emplace(newHash, newMsg);
+		return;
+	}
+	else {
+		for(int i = 0; i < syns[num].size(); i++) {
+			string msg;
+			if (syns[num][i] == "." || syns[num][i] == "," || syns[num][i] == "?" || syns[num][i] == "!") {
+				msg = newMsg;
+				msg.pop_back();
+				msg += syns[num][i] + " ";
+			}
+			else {
+				msg = newMsg + syns[num][i] + " ";
+			}
+			makeMap(syns, num+1, targetHash, msg, md);
+		}
+	}
+	return;
+}
+
+void findCollision(vector<vector<string> > syns, int num, const stringmap& targetHash, string newMsg, MD2 md){
+	if(syns.size() == num){
+		string newHash = md.hash(newMsg).substr(0,9);
+		stringmap::const_iterator got = targetHash.find(newHash);
+		if (got != targetHash.end()){
+			cout << newMsg << "\n" << got->second << "\n" << got->first << "\n";
+		}
+		return;
+	}
+	else {
+		for(int i = 0; i < syns[num].size(); i++) {
+			string msg;
+			if (syns[num][i] == "." || syns[num][i] == "," || syns[num][i] == "?" || syns[num][i] == "!") {
+				msg = newMsg;
+				msg.pop_back();
+				msg += syns[num][i] + " ";
+			}
+			else {
+				msg = newMsg + syns[num][i] + " ";
+			}
+			findCollision(syns, num+1, targetHash, msg, md);
+		}
+	}
+	return;
+}
+int main()
+{
+	string message, newmessage;
+cout << "Enter original sentence:" << endl;
+	getline(cin, message);
+cout << "New sentence:" << endl;
+	getline(cin, newmessage);
+
+	vector<string> words;
+	words.push_back("");
+	words[0] = tolower(message[0]);
+
+// Create vector of words in message
+	int count = 0;
+	for(int i = 1; i < message.length(); i++){
+		if(message[i] == ' '){
+			count++;
+			words.push_back("");
+			words[count] = tolower(message[++i]);
+		}
+		else if((message[i] == ',' || message[i] == '.') || message[i] == '?' || message[i] == '!'){
+			count++;
+			words.push_back("");
+			words[count] = message[i];
+		}
+		else
+			words[count].push_back(tolower(message[i]));
+	}
+
+	long unsigned int poss = 1;
+// Create vector<vector<string>> of synonyms
+	Thesaurus dino;
+
+	vector<vector<string> > syns;
+	for(int i = 0; i < words.size(); i++){
+		syns.push_back(dino.search(words[i]));
+	}
+	for(int i = 0; i < syns.size(); i++){
+		unsigned int subposs = 0;
+		for(int j = 0; j < syns[i].size(); j++){
+			cout << syns[i][j] << " ";
+			subposs++;
+		}
+		poss *= subposs;
+		cout << "\n";
+	}
+
+cout <<"make new words" << endl;
+// Create vector of words in message
+	words.clear();
+	words.push_back("");
+	words[0] = tolower(message[0]);
+	count = 0;
+	for(int i = 1; i < newmessage.length(); i++){
+		if(newmessage[i] == ' '){
+			count++;
+			words.push_back("");
+			words[count] = tolower(newmessage[++i]);
+		}
+		else if((newmessage[i] == ',' || newmessage[i] == '.') || newmessage[i] == '?' || newmessage[i] == '!'){
+			count++;
+			words.push_back("");
+			words[count] = newmessage[i];
+		}
+		else
+			words[count].push_back(tolower(newmessage[i]));
+	}
+cout << "make syns2" << endl;
+// Create vector<vector<string>> of synonyms
+	long unsigned int poss2 = 1;	
+	vector<vector<string> > syns2;
+
+	for(int i = 0; i < words.size(); i++){
+		syns2.push_back(dino.search(words[i]));
+	}
+	for(int i = 0; i < syns2.size(); i++){
+		unsigned int subposs = 0;
+		for(int j = 0; j < syns2[i].size(); j++){
+			cout << syns2[i][j] << " ";
+			subposs++;
+		}
+		poss2 *= subposs;
+		cout << "\n";
+	}
+	long long unsigned int posses = poss*poss2;
+	cout << poss << " " << poss2 << " " << posses << "\n";
+	MD2 hasher(4327543257);							// Generate random key
+
+	stringmap target;
+
+	long int n = 0;
+
+	if(poss <= poss2) {
+		makeMap(syns, 0, target, "", hasher);
+		cout << "map1 made" << "\n";
+		findCollision(syns2, 0, target, "", hasher);
+	}
+	else{
+		makeMap(syns2, 0, target, "", hasher);
+		cout << "map2 made" << "\n";
+		findCollision(syns, 0, target, "", hasher);
+	}
+
+
+
+	return 0;
+}
+
